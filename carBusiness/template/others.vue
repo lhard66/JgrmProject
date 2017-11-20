5c2efbe327a7ce71a7ab636976389e18ef0bf6fd
Liu TianQing <lhard@qq.com>
2017-11-20 15:29:12 +0800

    car-business模版

---
+++ carBusiness/template/others.vue
@@ -0,0 +1,7 @@
+<c-dialog title="详情" :visible.sync="dialog.detail">
+  <detail v-if="dialog.detail"></detail>
+</c-dialog>
+
+<c-dialog title="卡内项目" :width="450" :visible.sync="dialog.number">
+  <num-operator v-if="dialog.number"></num-operator>
+</c-dialog>
