20dc18d39013fa29e97a95fe2a769e21fbf9ed75
Liu TianQing <lhard@qq.com>
2018-01-11 18:24:00 +0800

    详情模板

---
+++ carBusiness/template/detail.vue
@@ -0,0 +1,65 @@
+<template>
+  <content-view :status="status" @reload="init">
+    <template v-if="status==AppStatus.READY">
+      <div class="general-detail">
+
+        <el-row :gutter="20">
+          <el-col :span="12">
+            <span>会员卡号：</span>
+            <span>123</span>
+          </el-col>
+          <el-col :span="12">
+            <span>会员等级：</span>
+            <span>8</span>
+          </el-col>
+        </el-row>
+        
+      </div>
+    </template>
+  </content-view>
+</template>
+
+<script>
+import { ContentView } from '@/components';
+import { AppStatus } from '@/common/enums';
+
+// import {  } from '@api';
+
+export default {
+  name: 'coupon-detail',
+  props: {
+    id: {
+      type: Number,
+      default: 11981,
+    },
+  },
+  components: {
+    ContentView,
+  },
+  data() {
+    return {
+      AppStatus,
+      status: AppStatus.READY,
+      page: null,
+    }
+  },
+  computed: {
+    //
+  },
+  mounted() {
+    this.init();
+  },
+  methods: {
+    init() {
+      //
+    },
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.el-row {
+  height: 30px;
+  line-height: 30px;
+}
+</style>
